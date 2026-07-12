da7c47202bc034a0ba86a9c46c8e2c94dc03adb7
neo <whereisyong@outlook.com>
2026-07-12 13:57:53 -0400

    Initial Commit

---
+++ twigin/twiginApp.swift
@@ -0,0 +1,17 @@
+//
+//  twiginApp.swift
+//  twigin
+//
+//  Created by Neo on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct twiginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
